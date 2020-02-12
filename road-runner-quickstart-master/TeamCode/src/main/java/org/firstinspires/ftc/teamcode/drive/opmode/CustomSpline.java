c62715deef9fff506d70e61115b7dac2d1e08c33
Roby <robertadrian85@gmail.com>
2020-02-12 11:04:55 +0200

    CustomSplines.Java new stuff

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CustomSpline.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CustomSpline.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -20,14 +21,28 @@ public class CustomSpline extends LinearOpMode {
         waitForStart();
 
         if (isStopRequested()) return;
-
+        drive.setPoseEstimate(new Pose2d(-36,-70,(float)Math.PI));
         drive.followTrajectorySync(
+
                 drive.trajectoryBuilder()
-                        .splineTo(new Pose2d(100, 0, 0))
-                        .splineTo(new Pose2d(100, 110, 0))
-                        .splineTo(new Pose2d(15, 110, 1))
-                        .splineTo(new Pose2d(0, 0, 0))
+                        .strafeTo(new Vector2d(-47,-35))
+                        .setReversed(true)
+                        .splineTo(new Pose2d(-20,-42,(float)Math.PI))
+                        .lineTo(new Vector2d(40,-40))
+                        .setReversed(false)
                         .build()
+
         );
+        sleep(1000);
+        drive.followTrajectorySync(
+
+                drive.trajectoryBuilder()
+                        .lineTo(new Vector2d(-50,-40))
+                        .build()
+
+        );
+
+
+
     }
 }
